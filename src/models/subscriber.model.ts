80d880daad5ab8f74d45455922c35f3271500969
Mukul P <mukul.p.mohan@gmail.com>
2021-05-06 23:46:34 +0530

    updated models

--- src/models/subscriber.model.ts
+++ src/models/subscriber.model.ts
@@ -5,18 +5,18 @@ import { Document } from 'mongoose';
   autoCreate: true,
   versionKey: false,
   strict: false,
-  timestamps: true,
 })
 export class SubscriberCollection extends Document {
   @Prop({ type: String, required: true, index: true })
   pincode: string;
-  @Prop({ type: String, required: true, index: true })
+  @Prop({ type: String, required: true })
   phoneNumber: string;
 }
 
 export const SubscriberCollectionSchema = SchemaFactory.createForClass(
   SubscriberCollection,
 );
+
 export interface ISubscriptionCollection {
   pincode: SubscriberCollection['pincode'];
   phoneNumber: SubscriberCollection['phoneNumber'];
